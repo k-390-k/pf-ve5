0de53e3603c67e8c3ea7d259c259dd30d9614c11
keisuke-sakuma <sakusuke0412@gmail.com>
2020-10-10 11:35:42 +0900

    作成:cooksコントローラ

---
+++ app/controllers/cooks_controller.rb
@@ -0,0 +1,13 @@
+class CooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'cooks/index'
+  get 'cooks/show'
+  get 'cooks/new'
+  get 'cooks/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
 end
