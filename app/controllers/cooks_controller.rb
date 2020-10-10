f7af9d980d139f662dd1f11a06be8b3d711a5ba6
keisuke-sakuma <sakusuke0412@gmail.com>
2020-10-10 12:25:09 +0900

    追加:記事投稿画面の実装

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -1,5 +1,6 @@
 class CooksController < ApplicationController
   def index
+    
   end
 
   def show
@@ -9,6 +10,19 @@ class CooksController < ApplicationController
     @cook = Cook.new
   end
 
+  def create
+    @cook = Cook.new(cook_params)
+    if @cook.save!
+       redirect_to @cook
+    end
+  end
+
   def edit
+  end
+
+
+  private
+  def cook_params
+    params.require(:cook).permit(:title,:description)
   end
 end
